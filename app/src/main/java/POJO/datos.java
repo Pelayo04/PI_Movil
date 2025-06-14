d7242ab9ecd2673c74db45fddb0fcda0d81e8dc1
Ulises Macías <umacias.1903@gmail.com>
2025-06-13 22:02:09 -0600

    arraylist, pojo

---
+++ app/src/main/java/POJO/datos.java
@@ -0,0 +1,73 @@
+package POJO;
+
+public class datos {
+
+    String nombreAlumno, apPat, apMat, telefono, herramienta, horaSalida, horaEntrega, nombreMaestro;
+
+    public int d, m ,a;
+    public int h, min;
+
+    public String getNombreAlumno() {
+        return nombreAlumno;
+    }
+
+    public void setNombreAlumno(String nombreAlumno) {
+        this.nombreAlumno = nombreAlumno;
+    }
+
+    public String getApPat() {
+        return apPat;
+    }
+
+    public void setApPat(String apPat) {
+        this.apPat = apPat;
+    }
+
+    public String getApMat() {
+        return apMat;
+    }
+
+    public void setApMat(String apMat) {
+        this.apMat = apMat;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setHoraSalida(String horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+
+    public String getHerramienta() {
+        return herramienta;
+    }
+
+    public void setHerramienta(String herramienta) {
+        this.herramienta = herramienta;
+    }
+
+    public String getHoraEntrega() {
+        return horaEntrega;
+    }
+
+    public void setHoraEntrega(String horaEntrega) {
+        this.horaEntrega = horaEntrega;
+    }
+
+    public String getNombreMaestro() {
+        return nombreMaestro;
+    }
+
+    public void setNombreMaestro(String nombreMaestro) {
+        this.nombreMaestro = nombreMaestro;
+    }
+}
